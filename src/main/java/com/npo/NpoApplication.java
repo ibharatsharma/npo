b08d37af4d95f458b949ffe2d572f65490ec1406
Bharat Sharma <ibharatsharma@gmail.com>
2025-03-15 18:44:04 +0000

    Initial Commit

---
+++ src/main/java/com/npo/NpoApplication.java
@@ -0,0 +1,13 @@
+package com.npo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npo/NpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
